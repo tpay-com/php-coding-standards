1b4c53690d877947f2146ed311945bd4b3b071f5
Paweł Stasicki <pawel.stasicki@tpay.com>
2025-06-12 14:58:11 +0200

    Disable PhpdocTagNoNamedArgumentsFixer for backward compatibility

--- src/Unwanted.php
+++ src/Unwanted.php
@@ -11,6 +11,7 @@ use PhpCsFixerCustomFixers\Fixer\NoImportFromGlobalNamespaceFixer;
 use PhpCsFixerCustomFixers\Fixer\NoNullableBooleanTypeFixer;
 use PhpCsFixerCustomFixers\Fixer\NoReferenceInFunctionDefinitionFixer;
 use PhpCsFixerCustomFixers\Fixer\PhpdocOnlyAllowedAnnotationsFixer;
+use PhpCsFixerCustomFixers\Fixer\PhpdocTagNoNamedArgumentsFixer;
 use PhpCsFixerCustomFixers\Fixer\PhpdocVarAnnotationToAssertFixer;
 use PhpCsFixerCustomFixers\Fixer\PromotedConstructorPropertyFixer;
 use PhpCsFixerCustomFixers\Fixer\ReadonlyPromotedPropertiesFixer;
@@ -36,15 +37,18 @@ final class Unwanted
                 'not_operator_with_space',
                 'not_operator_with_successor_space',
                 'octal_notation',
+                'php_unit_attributes',
                 'php_unit_internal_class',
                 'php_unit_size_class',
                 'php_unit_strict',
                 'php_unit_test_class_requires_covers',
                 'phpdoc_summary',
                 'phpdoc_to_property_type',
+                'phpdoc_to_return_type',
                 'single_line_comment_spacing',
                 'single_line_throw',
                 'strict_param',
+                'string_implicit_backslashes',
                 'void_return',
                 ConstructorEmptyBracesFixer::name(),
                 DeclareAfterOpeningTagFixer::name(),
@@ -53,6 +57,7 @@ final class Unwanted
                 NoNullableBooleanTypeFixer::name(),
                 NoReferenceInFunctionDefinitionFixer::name(),
                 PhpdocOnlyAllowedAnnotationsFixer::name(),
+                PhpdocTagNoNamedArgumentsFixer::name(),
                 PhpdocVarAnnotationToAssertFixer::name(),
                 PromotedConstructorPropertyFixer::name(),
                 ReadonlyPromotedPropertiesFixer::name(),
